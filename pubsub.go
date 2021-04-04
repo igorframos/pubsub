ee7c7dc56533de1b9820414debaa3b5625195751
Igor Ramos <igor.ramos@vtex.com.br>
2021-04-04 02:21:57 -0300

    First version
    There is a lot to polish, but this will do for now.

---
+++ pubsub.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+)
+
+type Controller struct {
+	// BufferSize is the size of the buffer for each channel. This represents the maximum number of unprocessed messages
+	// before the subscription will no longer receive new publications.
+	BufferSize int
+
+	// subs stores all the subscriptions for each topic. It is used to publish to all topics.
+	subs map[string][]*subscription
+
+	// subByID allows us to close the channel when someone is trying to get rid of a subscription.
+	subByID map[uint64]*subscription
+
+	// nextID will allow us to give a unique ID to each subscription so we can find it in all topics.
+	nextID uint64
+
+	// mutex is used for mutual exclusion when altering subs.
+	mutex sync.RWMutex
+}
+
+func New(bufferSize int) *Controller {
+	return &Controller{
+		BufferSize: bufferSize,
+		subs:       make(map[string][]*subscription),
+		subByID:    make(map[uint64]*subscription),
+	}
+}
+
+type Subscription interface {
+	ID() uint64
+	Topics() []string
+	C() <-chan interface{}
+}
+
+func (c *Controller) Publish(topic string, data interface{}) error {
+	var errors []string
+	for _, s := range c.subs[topic] {
+		if err := nonBlockingSend(s.c, data); err != nil {
+			errors = append(errors, fmt.Sprintf("Subscription %d: %v", s.ID(), err))
+		}
+	}
+
+	if len(errors) == 0 {
+		return nil
+	}
+
+	if len(errors) == 1 {
+		return fmt.Errorf("1 error when publishing to topic %s: %v", topic, errors[0])
+	}
+
+	return fmt.Errorf("%d errors publishing to topic %q:\n   %v", len(errors), topic, strings.Join(errors, "\n   "))
+}
+
+func (c *Controller) Subscribe(topics []string) Subscription {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.nextID++
+	s := &subscription{
+		id:     c.nextID,
+		topics: topics,
+		c:      make(chan interface{}, c.BufferSize),
+	}
+
+	c.subByID[s.ID()] = s
+	for _, topic := range topics {
+		c.subs[topic] = append(c.subs[topic], s)
+	}
+
+	return s
+}
+
+func (c *Controller) Unsubscribe(s Subscription) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	for _, t := range s.Topics() {
+		c.subs[t] = purge(c.subs[t], s)
+		if len(c.subs[t]) == 0 {
+			delete(c.subs, t)
+		}
+	}
+
+	close(c.subByID[s.ID()].c)
+	delete(c.subByID, s.ID())
+}
+
+// subscription implements Subscription. This ensures the struct is immutable and that only this package is able to send
+// in the channel held by the struct. Anyone outside this package will only see a Subscription, which returns a
+// receive-only channel.
+type subscription struct {
+	id     uint64
+	topics []string
+	c      chan interface{}
+}
+
+// ID is a getter for the id property.
+func (s *subscription) ID() uint64 {
+	return s.id
+}
+
+// Topics is a getter for the topics property.
+func (s *subscription) Topics() []string {
+	return s.topics
+}
+
+// C is a getter for the c property.
+func (s *subscription) C() <-chan interface{} {
+	return s.c
+}
+
+func nonBlockingSend(c chan<- interface{}, data interface{}) error {
+	select {
+	case c <- data:
+		return nil
+	default:
+		return fmt.Errorf("Channel was not ready")
+	}
+}
+
+func purge(subs []*subscription, s Subscription) []*subscription {
+	for i, sub := range subs {
+		if sub.ID() == s.ID() {
+			return append(subs[:i], subs[i+1:]...)
+		}
+	}
+
+	return subs
+}
---
+++ pubsub_test.go
@@ -0,0 +1,207 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+const (
+	topic1 = "topic1"
+	topic2 = "topic2"
+	topic3 = "topic3"
+
+	data1 = "test data 1"
+	data2 = "test data 2"
+	data3 = "test data 3"
+
+	bufferSize = 2
+)
+
+var (
+	pubsub = New(bufferSize)
+)
+
+func TestSingleSubscription(t *testing.T) {
+	s := pubsub.Subscribe([]string{topic1})
+
+	t.Run("Receives publication in the same topic", func(t *testing.T) {
+		pubsub.Publish(topic1, data1)
+		if err := receiveString(s, data1); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Does not receive publication in a different topic", func(t *testing.T) {
+		pubsub.Publish(topic2, data2)
+		if err := dontReceive(s); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Can be removed", func(t *testing.T) {
+		pubsub.Unsubscribe(s)
+
+		pubsub.Publish(topic1, data1)
+		if err := dontReceive(s); err != nil {
+			t.Error(err)
+		}
+	})
+}
+
+func TestSubscriptionForMultipleTopics(t *testing.T) {
+	s := pubsub.Subscribe([]string{topic1, topic2})
+
+	t.Run("Receives publication in both topics", func(t *testing.T) {
+		pubsub.Publish(topic1, data1)
+		if err := receiveString(s, data1); err != nil {
+			t.Error(err)
+		}
+
+		pubsub.Publish(topic2, data2)
+		if err := receiveString(s, data2); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Does not receive publication in a different topic", func(t *testing.T) {
+		pubsub.Publish(topic3, data3)
+		if err := dontReceive(s); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Can be removed", func(t *testing.T) {
+		pubsub.Unsubscribe(s)
+
+		pubsub.Publish(topic1, data1)
+		if err := dontReceive(s); err != nil {
+			t.Error(err)
+		}
+
+		pubsub.Publish(topic2, data2)
+		if err := dontReceive(s); err != nil {
+			t.Error(err)
+		}
+	})
+}
+
+func TestMultipleSubscriptions(t *testing.T) {
+	s1 := pubsub.Subscribe([]string{topic1, topic2})
+	s2 := pubsub.Subscribe([]string{topic2})
+
+	t.Run("Only subscriptions for a topic receive publications for it", func(t *testing.T) {
+		pubsub.Publish(topic1, data1)
+
+		if err := receiveString(s1, data1); err != nil {
+			t.Error(err)
+		}
+
+		if err := dontReceive(s2); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("All subscriptions for a topic receive publications for it", func(t *testing.T) {
+		pubsub.Publish(topic2, data2)
+
+		if err := receiveString(s1, data2); err != nil {
+			t.Error(err)
+		}
+
+		if err := receiveString(s2, data2); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Removing one subscription does not affect the other", func(t *testing.T) {
+		pubsub.Unsubscribe(s2)
+
+		pubsub.Publish(topic1, data1)
+
+		if err := receiveString(s1, data1); err != nil {
+			t.Error(err)
+		}
+
+		if err := dontReceive(s2); err != nil {
+			t.Error(err)
+		}
+
+		pubsub.Publish(topic2, data2)
+
+		if err := receiveString(s1, data2); err != nil {
+			t.Error(err)
+		}
+
+		if err := dontReceive(s2); err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("All subscriptions can be removed", func(t *testing.T) {
+		pubsub.Unsubscribe(s1)
+
+		pubsub.Publish(topic2, data2)
+
+		if err := dontReceive(s1); err != nil {
+			t.Error(err)
+		}
+
+		if err := dontReceive(s2); err != nil {
+			t.Error(err)
+		}
+	})
+}
+
+func TestBuffer(t *testing.T) {
+	_ = pubsub.Subscribe([]string{topic1, topic2})
+
+	t.Run("We can publish without error until the buffer is full", func(t *testing.T) {
+		for i := 0; i < bufferSize; i++ {
+			if err := pubsub.Publish(topic1, data1); err != nil {
+				t.Error(err)
+			}
+		}
+	})
+
+	t.Run("When the buffer is full, Publish() will report an error", func(t *testing.T) {
+		if err := pubsub.Publish(topic1, data1); err == nil {
+			t.Error("Expected to get an error from the buffer being full, but publication was accepted")
+		}
+	})
+}
+
+func receiveString(s Subscription, expected string) error {
+	select {
+	case data, ok := <-s.C():
+		if !ok {
+			return fmt.Errorf("Expeceted to receive data from subscription %d, but the channel is closed", s.ID())
+		}
+
+		str, ok := data.(string)
+		if !ok {
+			return fmt.Errorf("Received data from subscription %d, but it is not a string", s.ID())
+		}
+
+		if str != expected {
+			return fmt.Errorf("Incorrect data received by subscription %d. Expected: %q. Got: %q", s.ID(), expected, str)
+		}
+	default:
+		return fmt.Errorf("Timed out waiting for string %q", expected)
+	}
+
+	return nil
+}
+
+func dontReceive(s Subscription) error {
+	select {
+	case data, ok := <-s.C():
+		if ok {
+			return fmt.Errorf("Expected not to receive data, but got %q", data)
+		}
+
+		// This is not a receive. It just means the channel has been closed.
+		return nil
+	default:
+		return nil
+	}
+}
